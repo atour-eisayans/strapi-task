8bba52151df7e1e5d42ee47d919d01dcb226e0db
Atour Eisayans <atour.eisayans@gmail.com>
2023-03-10 02:39:17 +0400

    some simple updates

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 This program has been tested with node:14.21.3
 
-After pulling the codes, create a .env file and add the following key to that file:
+After pulling the codes, create a .env file in root directory and add the following key to that file:
 `SENDGRID_API_KEY=...`
 
 Postman collection and environment has been added.
-
 Please see the following link for more information:
 https://docs.google.com/document/d/1TOlyKNkhCFDfpoQFvk-kKc1X3a6xzaydBp2jdepGNGg/edit?usp=sharing
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,20 +1,19 @@
 module.exports = async () => {
-  const promises = [];
   const authenticated = await strapi
     .query("role", "users-permissions")
     .findOne({ type: "authenticated" });
-  authenticated.permissions.forEach((permission) => {
-    if (
-      permission.type === "application" &&
-      permission.controller === "meeting" &&
-      permission.action !== "count"
-    ) {
-      promises.push(
-        strapi
-          .query("permission", "users-permissions")
-          .update({ id: permission.id }, { ...permission, enabled: true })
-      );
-    }
-  });
+  const promises = authenticated.permissions
+    .filter(
+      (permission) =>
+        permission.type === "application" &&
+        permission.controller === "meeting" &&
+        permission.action !== "count"
+    )
+    .map((permission) =>
+      strapi
+        .query("permission", "users-permissions")
+        .update({ id: permission.id }, { ...permission, enabled: true })
+    );
+
   await Promise.all(promises);
 };
