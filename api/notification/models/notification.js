edf50e37e27cbf49d5f51e353ba3107fd99bd414
Atour Eisayans <atour.eisayans@gmail.com>
2023-03-10 10:13:38 +0400

    added error handler for notifications email sender

--- api/notification/models/notification.js
+++ api/notification/models/notification.js
@@ -3,14 +3,20 @@ module.exports = {
     async afterCreate(result, data) {
       const userEmail = result.userId.email;
       if (userEmail) {
-        // We did not use await to response faster to the client
-        // if it's necessary that email be sent for the rest of the process
+        // We did not use await in order to response faster to the client
+        // if it's necessary that email be sent
         // then we must use await and handle it
-        strapi.plugins["email"].services.email.send({
-          to: userEmail,
-          subject: "New meeting has been created",
-          text: result.notificationText,
-        });
+        strapi.plugins["email"].services.email
+          .send({
+            to: userEmail,
+            subject: "New meeting has been created",
+            text: result.notificationText,
+          })
+          .catch((err) => {
+            if (err.response) {
+              strapi.log.error("failed to send email", err.response.body);
+            }
+          });
       }
     },
   },
